ae860e22a0058c92be055980977309471ea08949
Egor Pozin <epozin@cinimex.ru>
2023-01-24 03:34:50 +0300

    first hw with xml config

---
+++ src/main/java/ru/epozin/otus/Main.java
@@ -0,0 +1,15 @@
+package ru.epozin.otus;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import ru.epozin.otus.service.QuestionsService;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("/spring-context.xml");
+        QuestionsService service = context.getBean(QuestionsService.class);
+        service.doQuiz();
+    }
+}
