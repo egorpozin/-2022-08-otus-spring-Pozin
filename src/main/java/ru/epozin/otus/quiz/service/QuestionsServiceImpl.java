923c65d2c4fdbfdef371aff13e1afc68a7e5d44f
epozin <epozin@cmx.ru>
2023-03-06 16:50:07 +0300

    fix pr

--- src/main/java/ru/epozin/otus/quiz/service/QuestionsServiceImpl.java
+++ src/main/java/ru/epozin/otus/quiz/service/QuestionsServiceImpl.java
@@ -26,19 +26,20 @@ public class QuestionsServiceImpl implements QuestionsService {
 
     }
 
+    @Override
     public void doQuiz() {
         askName();
         int correctAnswers = quizProcess(dao.getQuestions());
         io.out(getResult(correctAnswers, requiredAnswers));
     }
 
-    public int quizProcess(List<Question> questionList) {
+    private int quizProcess(List<Question> questionList) {
         int correctAnswers = 0;
         for (Question question : questionList) {
-            io.out("Question #" + question.getQuestionNumber());
+            io.out(messageSource.getMessage("message.question", new String[]{String.valueOf(question.getQuestionNumber())}, locale));
             io.out(question.getQuestionText());
             if (!question.getOptions().isEmpty())
-                System.out.println(question.getOptions());
+                io.out(question.getOptions());
             String answer = io.readString();
             if (checkAnswer(question, answer)) {
                 correctAnswers++;
@@ -47,19 +48,19 @@ public class QuestionsServiceImpl implements QuestionsService {
         return correctAnswers;
     }
 
-    public void askName() {
+    private void askName() {
         io.out(messageSource.getMessage("message.name", null, locale));
         String name = io.readString();
         io.out(messageSource.getMessage("message.hello", new String[]{name}, locale));
         io.out(messageSource.getMessage("message.quiz", new String[]{String.valueOf(requiredAnswers)}, locale));
     }
 
-    public boolean checkAnswer(Question question, String answer) {
+    private boolean checkAnswer(Question question, String answer) {
         return question.getRightAnswer().equalsIgnoreCase(answer);
 
     }
 
-    public String getResult(Integer correctAnswers, Integer requiredAnswer) {
+    private String getResult(Integer correctAnswers, Integer requiredAnswer) {
         if (correctAnswers >= requiredAnswer)
             return messageSource.getMessage("message.success", new String[]{String.valueOf(correctAnswers)}, locale);
         else return messageSource.getMessage("message.failure", new String[]{String.valueOf(correctAnswers)}, locale);
